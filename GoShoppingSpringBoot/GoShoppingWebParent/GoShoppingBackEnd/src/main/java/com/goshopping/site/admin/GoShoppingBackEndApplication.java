154227d907f60e954d2636dd6b276589c4b1a717
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-02-14 18:22:38 +0000

    Criação do Role & RoleRepository (Admin Page)

--- GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/GoShoppingBackEndApplication.java
+++ GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/GoShoppingBackEndApplication.java
@@ -2,11 +2,18 @@ package com.goshopping.site.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
+@EntityScan({"com.goshopping.common.entity", "com.goshopping.site.admin.user"})
+
+//@ContextConfiguration({"classpath*:com/goshopping/common/entity",
+									//"classpath*:com/goshopping/site/admin/user"})
 public class GoShoppingBackEndApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GoShoppingBackEndApplication.class, args);
 	}
 
