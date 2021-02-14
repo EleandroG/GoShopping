1d5dd5cd54ed06dde3afcd7e8fffee4f27a942ce
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-02-14 18:58:58 +0000

    Testing Database

--- GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/GoShoppingBackEndApplication.java
+++ GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/GoShoppingBackEndApplication.java
@@ -7,9 +7,6 @@ import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
 @EntityScan({"com.goshopping.common.entity", "com.goshopping.site.admin.user"})
-
-//@ContextConfiguration({"classpath*:com/goshopping/common/entity",
-									//"classpath*:com/goshopping/site/admin/user"})
 public class GoShoppingBackEndApplication {
 
 	public static void main(String[] args) {
--- GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/test/java/com/goshopping/site/admin/user/RoleRepositoryTests.java
+++ GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/test/java/com/goshopping/site/admin/user/RoleRepositoryTests.java
@@ -2,13 +2,16 @@ package com.goshopping.site.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import com.goshopping.common.entity.Role;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
 
     @Autowired
@@ -16,8 +19,22 @@ public class RoleRepositoryTests {
 
     @Test
     public void testCreateFirstRole() {
-        Role roleAdmin = new Role("Admin", "Manage Everything");
+        Role roleAdmin = new Role("Admin", "Manage everything");
         Role savedRole = repo.save(roleAdmin);
         assertThat(savedRole.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void testCreateRestRoles() {
+        Role roleSalesPerson = new Role("Salesperson", "Manage product prices, orders, etc");
+        Role savedRole2 = repo.save(roleSalesPerson);
+        Role roleEditor = new Role("Editor", "Manage categories, articles, etc");
+        Role savedRole3 = repo.save(roleEditor);
+        Role roleShipper = new Role("Shipper", "Manage shipping order, order status, etc");
+        Role savedRole4 = repo.save(roleShipper);
+        Role roleAssistant = new Role("Assistant", "Manage reviews, Q&A, etc.");
+        Role savedRole5 = repo.save(roleAssistant);
+        //repo.saveAll(List.of(roleSalesPerson, roleEditor, roleShipper, roleAssistant));
+    }
+
 }
