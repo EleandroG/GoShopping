02a5c894575d8997bab671ea5a6238c8327eae81
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-02-15 18:37:34 +0000

    NewUser with UserForm

--- GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/user/UserController.java
+++ GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/user/UserController.java
@@ -1,10 +1,13 @@
 package com.goshopping.site.admin.user;
 
+import com.goshopping.common.entity.Role;
 import com.goshopping.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -14,10 +17,31 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @GetMapping("/users")
+    @GetMapping("/users") //url para listar os users com a pag HTML5
     public String listAll(Model model) {
-        List<User>listUsers = service.listAll();
+        List<User> listUsers = service.listAll();
         model.addAttribute("listUsers", listUsers);
-        return "users";
+        return "users"; //ficheiro que devolve
+    }
+
+    @GetMapping("/users/new") //url que devolve a pag HTML5
+    public String newUser(Model model) {
+        List<Role> listRoles = service.listRoles();
+        model.addAttribute("listRoles", listRoles);
+
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+
+        return "user_form"; //ficheiro que devolve
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.print(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully!");
+        return "redirect:/users";
     }
 }
--- GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/user/UserService.java
+++ GoShoppingSpringBoot/GoShoppingWebParent/GoShoppingBackEnd/src/main/java/com/goshopping/site/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.goshopping.site.admin.user;
 
+import com.goshopping.common.entity.Role;
 import com.goshopping.common.entity.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
 
     public List<User> listAll() {
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save(User user) {
+        userRepo.save(user);
     }
 }
